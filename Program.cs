59fc7b2118439d45d8bedcd78b178792c5530ee5
justesma <148981651+justesma@users.noreply.github.com>
2023-10-25 17:20:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       int deger1, deger2, toplam;
+        Console.WriteLine("ilk değerini giriniz :");
+        deger1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("ikinci değeri giriniz :");
+        deger2 = Convert.ToInt32(Console.ReadLine());
+        toplam = deger1 + deger2;
+        Console.WriteLine("sonuç = {0} " + toplam);
+        Console.ReadKey();
+       
+    }
+}
