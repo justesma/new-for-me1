2f2e6577e7f14f2a32ede8bbff3cf47c4d8bdae9
justesma <148981651+justesma@users.noreply.github.com>
2023-12-31 23:44:44 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,14 +2,20 @@
 {
     private static void Main(string[] args)
     {
-       int deger1, deger2, toplam;
-        Console.WriteLine("ilk değerini giriniz :");
-        deger1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("ikinci değeri giriniz :");
-        deger2 = Convert.ToInt32(Console.ReadLine());
-        toplam = deger1 + deger2;
-        Console.WriteLine("sonuç = {0} " + toplam);
-        Console.ReadKey();
        
+      
+        int deger1, deger2, toplam;
+
+        Console.WriteLine("İlk değeri giriniz:");
+        deger1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("İkinci değeri giriniz:");
+        deger2 = Convert.ToInt32(Console.ReadLine());
+
+        toplam = deger1 + deger2;
+
+        Console.WriteLine("Sonuç = {0}", toplam);
+
+        Console.ReadKey();
     }
 }
